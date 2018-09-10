1dce6f999b40a9fec0102d74665ad6affbacab85
Екатерина <liberty_12@mail.ru>
2018-09-10 10:37:30 +0300

    Add renderCard and renderPin

--- js/map.js
+++ js/map.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var PIN_HALF_WIDTH = 25;
+var PIN_HEIGHT = 70;
+
 var avatars = ['1', '2', '3', '4', '5', '6', '7', '8'];
 var titles = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец',
   'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик',
@@ -10,6 +13,12 @@ var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditio
 var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var defenitionOfTypes = {
+  'palace': 'Дворец',
+  'flat': 'Квартира',
+  'house': 'Дом',
+  'bungalo': 'Бунгало'
+};
 
 var getRandomInArray = function (arr, isNoRepeated) {
   var randomPosition = Math.floor((Math.random() * arr.length));
@@ -44,13 +53,15 @@ var compareRule = function () {
 var renderAdvertFeatures = function (avatarFeature, titleFeature, typeFeature, timeFeature, attribute, photoFeature) {
   var adverts = [];
   for (var i = 1; i <= 8; i++) {
+    var x = getRandomInteger(0, 1200);
+    var y = getRandomInteger(130, 630);
     var advert = {
       'author': {
-        'avatar': 'img/avatars/user' + '0' + getRandomInteger(1, 8).toString() + '.png'
+        'avatar': 'img/avatars/user' + '0' + getRandomInArray(avatarFeature, true) + '.png'
       },
       'offer': {
         'title': getRandomInArray(titleFeature, true),
-        'address': '',
+        'address': x.toString() + ', ' + y.toString(),
         'price': getRandomInteger(1000, 1000000),
         'type': getRandomInArray(typeFeature),
         'rooms': getRandomInteger(1, 5),
@@ -62,8 +73,8 @@ var renderAdvertFeatures = function (avatarFeature, titleFeature, typeFeature, t
         'photos': photoFeature.sort(compareRule)
       },
       'location': {
-        'x': '',
-        'y': getRandomInteger(130, 630)
+        'x': x,
+        'y': y
       }
     };
     adverts.push(advert);
@@ -72,7 +83,32 @@ var renderAdvertFeatures = function (avatarFeature, titleFeature, typeFeature, t
   return adverts;
 };
 
-var advertisments = renderAdvertFeatures(avatars, titles, types, time, features, photos);
+var renderPin = function (card, template) {
+  var pinElement = template.cloneNode(true);
 
+  pinElement.style.left = (card.location.x - PIN_HALF_WIDTH).toString() + 'px';
+  pinElement.style.top = (card.location.y - PIN_HEIGHT).toString() + 'px';
+  pinElement.querySelector('img').src = card.author.avatar;
+  pinElement.querySelector('img').alt = card.offer.title;
 
-// var map = document.querySelector('.map').classList.remove('map--faded');
+  return pinElement;
+};
+
+var renderCard = function (card, template, defenition) {
+  var cardElement = template.cloneNode(true);
+
+  cardElement.querySelector('.popup__avatar').src = card.author.avatar;
+  cardElement.querySelector('.popup__title').textContent = card.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = card.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = card.offer.price + '₽/ночь';
+  cardElement.querySelector('.popup__type').textContent = defenition[card.offer.type];
+  cardElement.querySelector('.popup__text--capacity').textContent = card.offer.rooms + ' комнаты для ' + card.offer.guest + ' гостей';
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
+  // cardElement.querySelector('.popup__feature').textContent = card.offer.features;
+  cardElement.querySelector('.popup__description').textContent = card.offer.description;
+  cardElement.querySelector('.popup__photo').src = card.offer.photos[0];
+  // cardElement.querySelector('.popup__photo').cloneNode(true).src = card.offer.photos[1];
+  // cardElement.querySelector('.popup__photo').cloneNode(true).src = card.offer.photos[2];
+
+  return cardElement;
+};
